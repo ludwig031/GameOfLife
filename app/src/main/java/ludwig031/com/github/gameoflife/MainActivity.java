7b39c52974e5c94764c069dcfacc5eab15aa241c
ludwig031 <borna.matijanic@gmail.com>
2017-08-09 21:43:52 +0200

    Initial commit

---
+++ app/src/main/java/ludwig031/com/github/gameoflife/MainActivity.java
@@ -0,0 +1,13 @@
+package ludwig031.com.github.gameoflife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
