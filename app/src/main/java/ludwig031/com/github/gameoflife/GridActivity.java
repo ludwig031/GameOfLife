3b04bd63847ab7afb2a0df57f82adafa7db6779a
ludwig031 <borna.matijanic@gmail.com>
2017-08-17 22:48:57 +0200

    add settings strings

--- app/src/main/java/ludwig031/com/github/gameoflife/GridActivity.java
+++ app/src/main/java/ludwig031/com/github/gameoflife/GridActivity.java
@@ -1,9 +1,11 @@
 package ludwig031.com.github.gameoflife;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class GridActivity extends Activity{
+
     /** Poziva se kod prvog kreiranja aktivnosti. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
