1c40e51ba6d7f20b4b21a563bd657616a8690b7d
ludwig031 <borna.matijanic@gmail.com>
2017-08-17 23:00:58 +0200

    create menu layout

--- app/src/main/java/ludwig031/com/github/gameoflife/GridActivity.java
+++ app/src/main/java/ludwig031/com/github/gameoflife/GridActivity.java
@@ -1,7 +1,6 @@
 package ludwig031.com.github.gameoflife;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class GridActivity extends Activity{
@@ -9,7 +8,7 @@ public class GridActivity extends Activity{
     /** Poziva se kod prvog kreiranja aktivnosti. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(Bundle savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.grid);
     }
 }
