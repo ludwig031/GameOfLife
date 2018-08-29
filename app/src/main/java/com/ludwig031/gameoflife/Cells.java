7cee99b9459d2455e4a8723bfe3023a457833369
Borna Ljudevit Matijanić <borna.matijanic@gmail.com>
2018-08-29 23:39:15 +0200

    Chenge method privilages

--- app/src/main/java/com/ludwig031/gameoflife/Cells.java
+++ app/src/main/java/com/ludwig031/gameoflife/Cells.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cells {
-    private Cells(int row, int col) {
+    public Cells(int row, int col) {
         this.row = row;
         this.col = col;
 
