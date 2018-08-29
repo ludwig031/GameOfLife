276968a5c60cbe6287aa2704051091f4f2b8d72e
Borna Ljudevit Matijanić <borna.matijanic@gmail.com>
2018-08-29 23:20:56 +0200

    Remove unused imports

--- app/src/main/java/com/ludwig031/gameoflife/CellAdapter.java
+++ app/src/main/java/com/ludwig031/gameoflife/CellAdapter.java
@@ -3,7 +3,6 @@ package com.ludwig031.gameoflife;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
